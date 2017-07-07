b82dfa61b28c505499f47ce98346d373af85dfb3
mjpark03 <mjpark03@gmail.com>
2017-07-07 12:28:22 +0900

    Modify Customers table definitions for correct column options Add querying module for Customers table

--- app/models/Customer.scala
+++ app/models/Customer.scala
@@ -1,12 +1,13 @@
 package models
 
-import java.sql.{Date, Time, Timestamp}
-import java.time.{LocalDate, LocalDateTime, LocalTime, ZoneOffset}
+import java.sql.Timestamp
 import javax.inject.Inject
 
 import play.api.db.slick.DatabaseConfigProvider
+
 import slick.jdbc.JdbcProfile
-import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.Future
 
 /**
   * Created by mijeongpark on 2017. 7. 5..
@@ -14,14 +15,14 @@ import slick.jdbc.MySQLProfile.api._
 
 case class Customer(
                      id: Long,
-                     name: String,
-                     authNumber: String,
-                     postNo: String,
-                     addr1: String,
-                     addr2: String,
+                     name: Option[String],
+                     authNumber: Option[String],
+                     postNo: Option[String],
+                     addr1: Option[String],
+                     addr2: Option[String],
                      createdAt: Timestamp,
                      updatedAt: Timestamp,
-                     deletedAt: Timestamp )
+                     deletedAt: Option[Timestamp] )
 
 class CustomerRepo @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) {
 
@@ -30,30 +31,34 @@ class CustomerRepo @Inject()(protected val dbConfigProvider: DatabaseConfigProvi
   import dbConfig.profile.api._
   private[models] val Customers = TableQuery[CustomersTable]
 
-  private[models] class CustomersTable(tag: Tag) extends Table[Customer](tag, "CUSTOMER") {
+  private def _findById(id: Long): DBIO[Option[Customer]] =
+    Customers.filter(_.id === id).result.headOption
+
+  def findById(id: Long): Future[Option[Customer]] =
+    db.run(_findById(id))
+
+  private[models] class CustomersTable(tag: Tag) extends Table[Customer](tag, "Customers") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
 
-    def name = column[String]("name")
+    def name = column[Option[String]]("name")
 
-    def authNumber = column[String]("authNumber")
+    def authNumber = column[Option[String]]("authNumber")
 
-    def postNo = column[String]("postNo")
+    def postNo = column[Option[String]]("postNo")
 
-    def addr1 = column[String]("addr1")
+    def addr1 = column[Option[String]]("addr1")
 
-    def addr2 = column[String]("addr2")
+    def addr2 = column[Option[String]]("addr2")
 
     def createdAt = column[Timestamp]("createdAt")
 
     def updatedAt = column[Timestamp]("updatedAt")
 
-    def deletedAt = column[Timestamp]("deletedAt")
+    def deletedAt = column[Option[Timestamp]]("deletedAt")
 
-    def * = (id, name, authNumber, postNo, addr1, addr2, createdAt, updatedAt, deletedAt) <> (Customer.tupled. Customer.unapply)
+    def * = (id, name, authNumber, postNo, addr1, addr2, createdAt, updatedAt, deletedAt) <> (Customer.tupled, Customer.unapply)
+//    def ? = (id.?, createdAt.?, updatedAt.?).shaped.<>({ r => import r._; _1.map(_ => Customer.tupled((_1.get, _2.get, _3.get))) }, (_: Any) => throw new Exception("Inserting into ? projection not supported."))
   }
-
-  def find(id: Long) =
-    db.run(Customers.filter(_.id === id).result.headOption)
 
 }
 

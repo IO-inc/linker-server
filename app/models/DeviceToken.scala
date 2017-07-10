cf42bac113ea962ec5b6124bfdb1c123c3f33b6b
mjpark03 <mjpark03@gmail.com>
2017-07-10 20:55:32 +0900

    [Major] Add db modules
    what?
      - to find access token info
      - to create new device token for push system

--- app/models/DeviceToken.scala
+++ app/models/DeviceToken.scala
@@ -1,35 +1,60 @@
+
 package models
 
 import java.sql.Timestamp
 import javax.inject.Inject
 
 import play.api.db.slick.DatabaseConfigProvider
-
 import slick.jdbc.JdbcProfile
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 /**
   * Created by Rachel on 2017. 7. 7..
   */
 
-case class DeviceToken (
-                       id: Long,
-                       customerDeviceId: Option[Long],
-                       deviceToken: Option[String],
-                       createdAt: Timestamp,
-                       updatedAt: Timestamp,
-                       deletedAt: Option[Timestamp] )
-
 class DeviceTokenRepo @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) {
 
   val dbConfig = dbConfigProvider.get[JdbcProfile]
   val db = dbConfig.db
+
+  import scala.concurrent.ExecutionContext.Implicits.global
   import dbConfig.profile.api._
+
   private[models] val DeviceTokens = TableQuery[DeviceTokensTable]
+  private[models] val AccessTokens = TableQuery[AccessTokensTable]
 
-  def insert(deviceToken: DeviceToken): DBIO[Long] =
-    DeviceTokens returning DeviceTokens.map(_.id) += deviceToken
+  private def __findByAccessToken(accessToken: String): DBIO[Option[AccessToken]] =
+    AccessTokens.filter(_.accessToken === accessToken).result.headOption
 
-  private[models] class DeviceTokensTable(tag: Tag) extends Table[DeviceToken](tag, "DeviceTokens") {
+  private def __insert(customerDeviceId: Option[Long], token: Option[String], now: Timestamp): DBIO[Long] =
+    DeviceTokens returning DeviceTokens.map(_.id) += DeviceToken(0, customerDeviceId, token, now, now, None)
+
+  def createDeviceToken(token: String, accessToken: String): Either[String, Long] = {
+    val now = new Timestamp(System.currentTimeMillis())
+
+    val actions = for {
+      accessTokenResult <- __findByAccessToken(accessToken)
+      deviceTokenResult <- {
+        if(accessTokenResult.isEmpty) DBIO.successful(None)
+        else __insert(accessTokenResult.get.customerDeviceId, Option(token), now)
+      }
+    } yield deviceTokenResult
+
+    // TODO: define error code
+    Await.result(
+      db.run(actions.transactionally).map { deviceTokenResult =>
+        deviceTokenResult match {
+          case n: Long => Right(n)
+          case None => Left("There is no access token")
+        }
+
+      }, Duration(3000, "millis"))
+  }
+
+  // TODO: separate table definitions
+  class DeviceTokensTable(tag: Tag) extends Table[DeviceToken](tag, "DeviceTokens") {
 
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def customerDeviceId = column[Option[Long]]("customerDeviceId")
@@ -40,4 +65,18 @@ class DeviceTokenRepo @Inject()(protected val dbConfigProvider: DatabaseConfigPr
 
     def * = (id, customerDeviceId, deviceToken, createdAt, updatedAt, deletedAt) <> (DeviceToken.tupled, DeviceToken.unapply)
   }
+
+  private[models] class AccessTokensTable(tag: Tag) extends Table[AccessToken](tag, "AccessTokens") {
+
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def customerDeviceId = column[Option[Long]]("customerDeviceId")
+    def accessToken = column[Option[String]]("accessToken")
+    def createdAt = column[Timestamp]("createdAt")
+    def updatedAt = column[Timestamp]("updatedAt")
+    def deletedAt = column[Option[Timestamp]]("deletedAt")
+
+    def * = (id, customerDeviceId, accessToken, createdAt, updatedAt, deletedAt) <> (AccessToken.tupled, AccessToken.unapply)
+
+  }
 }
+

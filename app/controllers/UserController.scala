eeeecd1f9b63af752b4a22966a0cfded8dc650a4
mjpark03 <mjpark03@gmail.com>
2017-07-10 14:36:01 +0900

    Add logic for handling create device token API

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,8 +1,9 @@
 package controllers
 
+import java.sql.Timestamp
 import javax.inject._
 
-import models.{CustomerRepo, Customer}
+import models._
 
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json.{JsValue, Writes, Json}
@@ -19,6 +20,7 @@ case class RequestInfo(url: String, method: String, headers: Map[String, String]
 class UserController @Inject()(dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   val customerRepo = new CustomerRepo(dbConfigProvider)
+  val accessTokenRepo = new AccessTokenRepo(dbConfigProvider)
 
   // TODO: saperate implicit writers
   implicit val implicitCommandDataWrites = new Writes[Customer] {
@@ -40,8 +42,20 @@ class UserController @Inject()(dbConfigProvider: DatabaseConfigProvider, cc: Con
 
   def createDeviceToken = Action.async { implicit request: Request[AnyContent] =>
 
-    println("request body: " + request.body)
-    Future.successful(Ok("fail"))
+
+    val jsonBody: Option[JsValue] = request.body.asJson
+
+    val authorization = request.headers.get("Authorization")
+    val accessToken = authorization.get.split(" ")(1)
+
+    val token = (jsonBody.get \ "token").as[String]
+    val now = new Timestamp(System.currentTimeMillis())
+
+    def futureResponse = for {
+      accessToken <- accessTokenRepo.findByAccessToken(token, accessToken)
+    } yield accessToken
+
+    Future.successful(Ok(Json.obj("status" -> "success")))
   }
 
 }

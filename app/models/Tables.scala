2db6e850f24485fde2e9392aa9dfe761a09f7d8e
mjpark03 <mjpark03@gmail.com>
2017-07-13 23:38:09 +0900

    Add table definition

--- app/models/Tables.scala
+++ app/models/Tables.scala
@@ -6,6 +6,26 @@ import java.sql.Timestamp
   * Created by mijeongpark on 2017. 7. 10..
   */
 
+case class Customer(
+                     id: Long,
+                     name: Option[String],
+                     authNumber: Option[String],
+                     postNo: Option[String],
+                     addr1: Option[String],
+                     addr2: Option[String],
+                     createdAt: Timestamp,
+                     updatedAt: Timestamp,
+                     deletedAt: Option[Timestamp])
+
+case class CustomerDevice(
+                           id: Long,
+                           customerId: Option[Long],
+                           uuid: Option[String],
+                           createdAt: Timestamp,
+                           updatedAt: Timestamp,
+                           deletedAt: Option[Timestamp]
+                         )
+
 case class AccessToken(
                         id: Long,
                         customerDeviceId: Option[Long],
@@ -14,10 +34,38 @@ case class AccessToken(
                         updatedAt: Timestamp,
                         deletedAt: Option[Timestamp])
 
-case class DeviceToken (
-                         id: Long,
-                         customerDeviceId: Option[Long],
-                         deviceToken: Option[String],
-                         createdAt: Timestamp,
-                         updatedAt: Timestamp,
-                         deletedAt: Option[Timestamp] )
+case class DeviceToken(
+                        id: Long,
+                        customerDeviceId: Option[Long],
+                        deviceToken: Option[String],
+                        createdAt: Timestamp,
+                        updatedAt: Timestamp,
+                        deletedAt: Option[Timestamp])
+
+case class Linker(
+                   id: Long,
+                   macAddress: Option[String],
+                   createdAt: Timestamp,
+                   updatedAt: Timestamp,
+                   deletedAt: Option[Timestamp]
+                 )
+
+case class Purchase(
+                     id: Long,
+                     linkerId: Option[Long],
+                     customerId: Option[Long],
+                     price: Option[String],
+                     warrantyDate: Option[Timestamp],
+                     createdAt: Timestamp,
+                     updatedAt: Timestamp,
+                     deletedAt: Option[Timestamp]
+                   )
+
+case class PurchaseOwner(
+                          id: Long,
+                          purchaseId: Option[Long],
+                          customerId: Option[Long],
+                          createdAt: Timestamp,
+                          updatedAt: Timestamp,
+                          deletedAt: Option[Timestamp]
+                        )

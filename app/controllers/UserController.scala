1815a8f147ec23f849d3d61697a773b42aa843fa
mjpark03 <mjpark03@gmail.com>
2017-07-13 17:01:35 +0900

    1. Remove unused function 2. Modify structure variables for db repository

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -15,10 +15,8 @@ import scala.concurrent.{ExecutionContext, Future}
   * Created by mijeongpark on 2017. 7. 5..
   */
 
-class UserController @Inject()(dbConfigProvider: DatabaseConfigProvider, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
-
-  val customerRepo = new CustomerRepo(dbConfigProvider)
-  val deviceTokenRepo = new DeviceTokenRepo(dbConfigProvider)
+class UserController @Inject()(cc: ControllerComponents,
+                               deviceTokenRepo: DeviceTokenRepo)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   // TODO: separate implicit writers
   implicit val implicitCommandDataWrites = new Writes[Customer] {
@@ -28,13 +26,6 @@ class UserController @Inject()(dbConfigProvider: DatabaseConfigProvider, cc: Con
         "name" -> model.name,
         "postNo" -> model.postNo
       )
-    }
-  }
-
-  def getCustomer(id: Long) = Action.async {
-    val future: Future[Option[Customer]] = customerRepo.findById(id)
-    future.map { model =>
-      Ok(Json.toJson(model))
     }
   }
 

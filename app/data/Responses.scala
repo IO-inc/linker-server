6cd2070bcbd88367086ba270f632c5c0d676c88d
mjpark03 <mjpark03@gmail.com>
2017-07-15 17:57:04 +0900

    Add response data format
    what?
      - CreateDeviceTokenResponse
      - GetUserDetailResponse

--- app/data/Responses.scala
+++ app/data/Responses.scala
@@ -1,18 +1,35 @@
 package data
 
+import java.sql.Timestamp
+
 import play.api.libs.json.{JsValue, Json, Writes}
 
 /**
   * Created by mijeongpark on 2017. 7. 10..
   */
-case class successResponse(status: String = "success", data: Option[JsValue])
-case class errorResponse(status: String = "error", message: String)
+case class SuccessResponse(status: String = "success", data: Option[JsValue])
+case class ErrorResponse(status: String = "error", message: String)
 case class CreateDeviceTokenResponse(id: Long)
+case class GetUserDetailResponse(
+                                id: Long,
+                                name: Option[String],
+                                phoneNumber: Option[String],
+                                authNumber: Option[String],
+                                postNo: Option[String],
+                                addr1: Option[String],
+                                addr2: Option[String],
+                                email: Option[String],
+                                createdAt: Timestamp,
+                                updatedAt: Timestamp,
+                                linkerList: Option[List[String]],
+                                switcherList: Option[List[String]],
+                                requestList: Option[List[String]]
+                                )
 
 object successResponse {
 
-  implicit val implicitSuccessResponse = new Writes[successResponse] {
-    def writes(response: successResponse): JsValue = {
+  implicit val implicitSuccessResponse = new Writes[SuccessResponse] {
+    def writes(response: SuccessResponse): JsValue = {
       Json.obj(
         "status" -> response.status,
         "data" -> response.data
@@ -23,8 +40,8 @@ object successResponse {
 
 object errorResponse {
 
-  implicit val implicitErrorResponse = new Writes[errorResponse] {
-    def writes(response: errorResponse): JsValue = {
+  implicit val implicitErrorResponse = new Writes[ErrorResponse] {
+    def writes(response: ErrorResponse): JsValue = {
       Json.obj(
         "status" -> response.status,
         "message" -> response.message
@@ -39,6 +56,29 @@ object CreateDeviceTokenResponse {
     def writes(response: CreateDeviceTokenResponse): JsValue = {
       Json.obj(
         "id" -> response.id
+      )
+    }
+  }
+}
+
+object GetUserDetailResponse {
+
+  implicit val implicitGetUserDetailResponse = new Writes[GetUserDetailResponse] {
+    def writes(response: GetUserDetailResponse): JsValue = {
+      Json.obj(
+        "id" -> response.id,
+        "phoneNumber" -> response.phoneNumber,
+        "name" -> response.name,
+        "authNumber" -> response.authNumber,
+        "postNo" -> response.postNo,
+        "addr1" -> response.addr1,
+        "addr2" -> response.addr2,
+        "email" -> response.email,
+        "createdAt" -> response.createdAt,
+        "updatedAt" -> response.updatedAt,
+        "linkerList" -> response.linkerList,
+        "switcherList" -> response.switcherList,
+        "requestList" -> response.requestList
       )
     }
   }

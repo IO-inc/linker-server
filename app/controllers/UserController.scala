fc23b0c6cbbac6d5f3375eed6995c4a9a2b0acc5
mjpark03 <mjpark03@gmail.com>
2017-07-17 19:39:20 +0900

    Separate modules for user service layer

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -4,8 +4,8 @@ import javax.inject._
 
 import models._
 import data._
+import services.{UserService, SwitcherService}
 
-import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json.{JsValue, Writes, Json}
 import play.api.mvc._
 
@@ -16,7 +16,10 @@ import scala.concurrent.{ExecutionContext, Future}
   */
 
 class UserController @Inject()(cc: ControllerComponents,
-                               deviceTokenRepo: DeviceTokenRepo)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+                               deviceTokenRepo: DeviceTokenRepo,
+                               accessTokenRepo: AccessTokenRepo,
+                               switcherService: SwitcherService,
+                               userService: UserService)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
   // TODO: separate implicit writers
   implicit val implicitCommandDataWrites = new Writes[Customer] {
@@ -29,6 +32,14 @@ class UserController @Inject()(cc: ControllerComponents,
     }
   }
 
+  case class response(macAddress: String)
+
+  implicit val responseWrites = new Writes[Linker] {
+    def writes(l: Linker) = Json.obj(
+      "macAddress" -> l.macAddress
+    )
+  }
+
   def createDeviceToken = Action.async { implicit request: Request[AnyContent] =>
 
     val jsonBody: Option[JsValue] = request.body.asJson
@@ -38,8 +49,26 @@ class UserController @Inject()(cc: ControllerComponents,
     val accessToken = authorization.get.split(" ")(1)
 
     deviceTokenRepo.createDeviceToken(token, accessToken) match {
-      case Right(id) => Future.successful(Ok(Json.toJson(successResponse("success", Option(Json.toJson(CreateDeviceTokenResponse(id)))))))
-      case Left(message) => Future.successful(Ok(Json.toJson(errorResponse("error", message))))
+      case Right(id) => Future.successful(Ok(Json.toJson(SuccessResponse("success", Option(Json.toJson(CreateDeviceTokenResponse(id)))))))
+      case Left(message) => Future.successful(Ok(Json.toJson(ErrorResponse("error", message))))
+    }
+  }
+
+  def getUserDetail = Action.async { implicit request: Request[AnyContent] =>
+
+    val authorization = request.headers.get("Authorization")
+    val accessToken = authorization.get.split(" ")(1)
+
+    userService.checkAccessToken(accessToken) match {
+      case Right(accessToken) => {
+        val (customer, linkerList) = userService.getUserDetail(accessToken)
+        // TODO: replace "Ra2uLPm0CTRQYXdzMglpbs+N7eLv4svrXEjd9YLACEI=" to access token parameter after modifying Switcher Server
+        val switcherList = switcherService.getSwitcherDetail("Ra2uLPm0CTRQYXdzMglpbs+N7eLv4svrXEjd9YLACEI=")
+        val userDetail = GetUserDetailResponse(customer, Option(linkerList.get.map(m => m.macAddress.get)), switcherList, None)
+
+        Future.successful(Ok(Json.toJson(SuccessResponse("success", Option(Json.toJson(userDetail))))))
+      }
+      case Left(message) => Future.successful(Ok(Json.toJson(ErrorResponse("error", message))))
     }
   }
 

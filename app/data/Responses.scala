50fa7a3ff04db6eeffe63ef7d1c0c63423f04dc1
mjpark03 <mjpark03@gmail.com>
2017-07-17 19:36:02 +0900

    Modify implicit writes for get user detail response

--- app/data/Responses.scala
+++ app/data/Responses.scala
@@ -1,7 +1,6 @@
 package data
 
-import java.sql.Timestamp
-
+import models.Customer
 import play.api.libs.json.{JsValue, Json, Writes}
 
 /**
@@ -11,22 +10,13 @@ case class SuccessResponse(status: String = "success", data: Option[JsValue])
 case class ErrorResponse(status: String = "error", message: String)
 case class CreateDeviceTokenResponse(id: Long)
 case class GetUserDetailResponse(
-                                id: Long,
-                                name: Option[String],
-                                phoneNumber: Option[String],
-                                authNumber: Option[String],
-                                postNo: Option[String],
-                                addr1: Option[String],
-                                addr2: Option[String],
-                                email: Option[String],
-                                createdAt: Timestamp,
-                                updatedAt: Timestamp,
+                                customer: Customer,
                                 linkerList: Option[List[String]],
                                 switcherList: Option[List[String]],
                                 requestList: Option[List[String]]
                                 )
 
-object successResponse {
+object SuccessResponse {
 
   implicit val implicitSuccessResponse = new Writes[SuccessResponse] {
     def writes(response: SuccessResponse): JsValue = {
@@ -38,7 +28,7 @@ object successResponse {
   }
 }
 
-object errorResponse {
+object ErrorResponse {
 
   implicit val implicitErrorResponse = new Writes[ErrorResponse] {
     def writes(response: ErrorResponse): JsValue = {
@@ -66,16 +56,16 @@ object GetUserDetailResponse {
   implicit val implicitGetUserDetailResponse = new Writes[GetUserDetailResponse] {
     def writes(response: GetUserDetailResponse): JsValue = {
       Json.obj(
-        "id" -> response.id,
-        "phoneNumber" -> response.phoneNumber,
-        "name" -> response.name,
-        "authNumber" -> response.authNumber,
-        "postNo" -> response.postNo,
-        "addr1" -> response.addr1,
-        "addr2" -> response.addr2,
-        "email" -> response.email,
-        "createdAt" -> response.createdAt,
-        "updatedAt" -> response.updatedAt,
+        "id" -> response.customer.id,
+        "phoneNumber" -> response.customer.phoneNumber,
+        "name" -> response.customer.name,
+        "authNumber" -> response.customer.authNumber,
+        "postNo" -> response.customer.postNo,
+        "addr1" -> response.customer.addr1,
+        "addr2" -> response.customer.addr2,
+        "email" -> response.customer.email,
+        "createdAt" -> response.customer.createdAt,
+        "updatedAt" -> response.customer.updatedAt,
         "linkerList" -> response.linkerList,
         "switcherList" -> response.switcherList,
         "requestList" -> response.requestList

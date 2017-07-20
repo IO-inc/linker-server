65c467ed24b19cc20e4d135397fb6b727fdae65b
mjpark03 <mjpark03@gmail.com>
2017-07-20 14:17:47 +0900

    Add error code
    what?
      - for sending sms
      - for db execution

--- app/data/Error.scala
+++ app/data/Error.scala
@@ -8,4 +8,7 @@ object ErrorMessage {
   val NO_ACCESS_TOKEN = "There is no access token"
   val NO_REQUEST_PARAMETER = " is empty in request parameters"
 
+  val FAIL_SMS_SEND = "sending SMS is failed"
+  val FAIL_DB_SQL_EXECUTION = "the execution of db sql is failed"
+
 }

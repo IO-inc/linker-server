9f1763cc7365230e2e220be6b8a2b159e0770d07
mjpark03 <mjpark03@gmail.com>
2017-07-22 20:20:02 +0900

    Add error message

--- app/data/Error.scala
+++ app/data/Error.scala
@@ -7,6 +7,9 @@ object ErrorMessage {
 
   val NO_ACCESS_TOKEN = "There is no access token"
   val NO_REQUEST_PARAMETER = " is empty in request parameters"
+  val NO_CUSTOMER = "There is no customer"
+
+  val INVALID_AUTH_NUMBER = "auth number is not correct"
 
   val FAIL_SMS_SEND = "sending SMS is failed"
   val FAIL_DB_SQL_EXECUTION = "the execution of db sql is failed"
